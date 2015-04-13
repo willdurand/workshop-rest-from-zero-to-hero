f32b191d00af77b253310e35ad45a6836b4dcaef
William Durand <will+git@drnd.me>
2015-04-13 17:04:31 +0200

    Implement a basic usage of the Serializer

--- src/Acme/HelloBundle/Controller/DefaultController.php
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -3,14 +3,30 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function helloAction($name)
+    public function helloAction(Request $request, $name)
     {
+        $data   = [ 'name' => $name ];
+        $format = $request->getRequestFormat();
+
+        if ('html' !== $format) {
+            $serializedData = $this
+                ->get('jms_serializer')
+                ->serialize($data, $format);
+
+            return new Response($serializedData, 200, [
+                // WARNING: Don't do this at home!
+                'Content-Type' => 'application/' . $format,
+            ]);
+        }
+
         return $this->render(
             'AcmeHelloBundle:Default:hello.html.twig',
-            array('name' => $name)
+            $data
         );
     }
 }
